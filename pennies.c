74eead325c95c57d655c86f9fb4a098d297a7756
bloftin4561 <bloftin4561@users.noreply.github.com>
2019-10-10 19:20:01 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- pennies.c
+++ pennies.c
@@ -5,21 +5,21 @@
 int main (void)
     
 {
-    int i = 0;
-    int day = get_int("Days in month: ");
-    int penny = get_int("Pennies on first day: ");
-    double total = penny;
-    if (27 < day && day < 32 && penny > 1)
+    double total = 0;
+    int day = 0;
+    int penny = 0;
+    do
     {
+    int i = 0;
+    day = get_int("Days in month: ");
+    penny = get_int("Pennies on first day: ");
+    total = penny;
     for (i = 0; i < day; i++)
     {
         total = total * 2;
     }
-    printf("$%.23f\n", total/100);
     }
-    else
-    {
-        printf("Retry\n");
-    }
-    }
+    while (day > 31 || day < 28 || penny < 1);
+        printf("$%.2f\n", total/100);
+}
     
