03e86f0c5df354cc4624f5ab53fa05ea047ddd52
bloftin4561 <bloftin4561@users.noreply.github.com>
2019-10-24 20:14:16 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ fahrenheit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    float C = atof(argv[1]);
+    printf("%.1f\n", (C * 9) / 5 + 32);
+}
