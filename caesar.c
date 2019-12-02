d3717d74e491fa3f4fa4b188e6cabd8327cfc600
bloftin4561 <bloftin4561@users.noreply.github.com>
2019-12-02 01:45:44 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- caesar.c
+++ caesar.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 #include <stdlib.h>
-
+//code wisely
 int main(int argc, string argv[])
 {
     if (argc != 2 || argv[1] < 0)
@@ -17,23 +17,23 @@ int main(int argc, string argv[])
     printf("ciphertext: ");
     for (i = 0; i < strlen(p); i++)
     {
-        if((p[i] < 91 && p[i] > 64) || (p[i] < 123 && p[i] > 96))
+        if ((p[i] < 91 && p[i] > 64) || (p[i] < 123 && p[i] > 96))
         {
-         c[i] = (p[i] + k);
+            c[i] = (p[i] + k);
         }
         else
         {
             c[i] = p[i];
         }
-             if ((c[i] > 90 && c[i] < 97) || (c[i] > 122))
-             {
-                 c[i] = c[i] - 26;
-              printf("%c", c[i]);
-             }
-             else
-             {
-                 printf("%c", c[i]);
-             }
+        if ((c[i] > 90 && c[i] < 97) || (c[i] > 122))
+        {
+            c[i] = c[i] - 26;
+            printf("%c", c[i]);
         }
+        else
+        {
+            printf("%c", c[i]);
+        }
+    }
     printf("\n");
 }
