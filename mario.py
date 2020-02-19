de449bf6855f5bd3af791bde22d573e73f2ddae1
bloftin4561 <bloftin4561@users.noreply.github.com>
2020-02-19 21:02:54 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -4,8 +4,9 @@ while True:
     n = get_int("Height: ")
     if n > 0 and n < 9:
         break
-
+# now stop and go play mario
 for i in range(n):
-    print(" " * (n-(i+1)), end = "")
+    print(" " * (n-(i+1)), end="")
 
     print("#" * (i+1))
+# no really go play the actual game
