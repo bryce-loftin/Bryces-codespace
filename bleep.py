e3654a9e8e6035b44485cc62d4525b4d7c3cef79
bloftin4561 <bloftin4561@users.noreply.github.com>
2020-02-27 20:49:00 +0000

    automated commit by check50 [check50=True]

--- bleep.py
+++ bleep.py
@@ -4,31 +4,34 @@ from cs50 import get_string
 
 def main():
 
- if len(sys.argv) != 2 :
-  print("Usage: python bleep.py dictionary")
-  exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: python bleep.py dictionary")
+        exit(1)
 
- infile = open(sys.argv[1], "r")
- bad = infile.read()
- ban = bad.split()
- cuss = bad.upper()
+    infile = open(sys.argv[1], "r")
+    bad = infile.read()
+    ban = bad.split()
+    cuss = bad.upper()
+    text = get_string("What message would you like to censor?\n")
+    word = text.split()
 
- text = get_string("What message would you like to censor?\n")
- word = text.split()
+    for i in word:
+
+        if i.upper() in cuss:
+            for n in i:
+                print("*", end="")
+            print(" ", end="")
+
+        elif i in ban:
+            for n in i:
+                print("*", end="")
+            print(" ", end="")
+
+        else:
+            print(f"{i} ", end="")
+
+    print("")
 
 
- for i in word:
-  if i.upper() in cuss:
-   for n in i:
-    print("*", end="")
-   print(" ", end= "")
-  elif i in ban:
-   for n in i:
-    print("*", end="")
-   print(" ", end= "")
-  else:
-   print(f"{i} ", end="")
-
- print("")
 if __name__ == "__main__":
     main()
